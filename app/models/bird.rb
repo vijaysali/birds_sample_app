793484ee6c744ff0d857472bfde81f3f2c1eebbb
vijay <vijay.sali@gmail.com>
2016-12-02 01:11:36 +0530

    1. initializer declared 2. bird document declared

---
+++ app/models/bird.rb
@@ -0,0 +1,8 @@
+class Bird
+  include Mongoid::Document
+  field :name, type: String
+  field :family, type: String
+  field :continents, type: Array
+  field :added, type: String
+  field :visible, type: Boolean
+end
