681ddb1426b8ebb4070fb82ce6cf29219ef10424
vijay <vijay.sali@gmail.com>
2016-12-02 01:09:25 +0530

    removed active-record dependencies

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,10 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+#require 'rails/all'
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "sprockets/railtie"
+require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -8,6 +12,7 @@ Bundler.require(*Rails.groups)
 
 module BirdApp
   class Application < Rails::Application
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -21,6 +26,7 @@ module BirdApp
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
+
+    #config.active_record.raise_in_transactional_callbacks = true
   end
 end
