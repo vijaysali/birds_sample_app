ad9e85c1730afb2c5bbef736ddebab7974f1aedc
vijay <vijay.sali@gmail.com>
2016-12-03 02:41:21 +0530

    model test cases updated

--- test/models/bird_test.rb
+++ test/models/bird_test.rb
@@ -23,4 +23,27 @@ class BirdTest < ActiveSupport::TestCase
     end
   end
 
+  # Introduce new fields apart from declared in models
+  # should raise exception
+  def test_create_unknown_attributes
+    input_value = {name: "test_bird", continents: @continents.sample(2), family: @families.sample, x: 1, y: 2}
+    assert_raises Mongoid::Errors::UnknownAttribute do
+      bird = Bird.new(input_value)
+      assert_equal true, bird.save!
+    end
+  end
+
+  def test_check_name_uniqueness
+    input_value = {name: "test_bird", continents: @continents.sample(2), family: @families.sample}
+    bird = Bird.new(input_value)
+
+    assert_equal true, bird.save!
+    assert_equal false, bird.visible
+
+    bird1 = Bird.new(input_value)
+    assert_raises Mongoid::Errors::Validations do
+      bird1.save!
+    end
+  end
+
 end
