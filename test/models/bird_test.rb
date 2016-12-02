c0ab47acac471fc051c6a048bd9b99adcc698ad3
vijay <vijay.sali@gmail.com>
2016-12-03 02:33:31 +0530

    unit test case for bird model

--- test/models/bird_test.rb
+++ test/models/bird_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class BirdTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @continents = ["Africa", "Antarctica", "Asia", "Australia", "Europe", "North America", "South America"]
+    @families   = ["Kingfishers", "Woodpeckers", "Creepers", "Mockingbirds", "Finches", "Flamingos"]
+  end
+
+  def teardown
+    Bird.destroy_all
+  end
+
+  # proper case
+  def test_create_bird
+    input_value = {name: "test_bird", continents: @continents.sample(2), family: @families.sample}
+    bird = Bird.new(input_value)
+
+    assert_equal true, bird.save!
+    assert_equal false, bird.visible
+    input_value.map do |attr_name, attr_value|
+      assert_equal attr_value, bird.send(attr_name)
+    end
+  end
+
 end
