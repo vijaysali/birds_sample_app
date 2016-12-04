fedf63600c5dbc56ec57578798cbac25301dfa4d
vijay <vijay.sali@gmail.com>
2016-12-04 07:38:58 +0530

    identation and documentation for bird model

--- app/models/bird.rb
+++ app/models/bird.rb
@@ -1,14 +1,16 @@
 class Bird
   include Mongoid::Document
-  field :name, type: String
-  field :family, type: String
-  field :continents, type: Array
-  field :added, type: DateTime, default: DateTime.now
-  field :visible, type: Boolean, default: false
+  field :name       , type: String
+  field :family     , type: String
+  field :continents , type: Array
+  field :added      , type: DateTime, default: DateTime.now
+  field :visible    , type: Boolean , default: false
 
+  # validations for attributes
   validates :name, :presence => true, uniqueness: true
   validates :family, :presence => true
   validates :continents, :presence => true
 
+  # scope to fetch all visible birds
   scope :visible, -> { where(visible: true)}
 end
