20ed23a3e3120b9d3c5eb646af84a82928e4fa3d
vijay <vijay.sali@gmail.com>
2016-12-04 18:37:09 +0530

    date time format change in serailizer

--- app/serializers/bird_serializer.rb
+++ app/serializers/bird_serializer.rb
@@ -4,4 +4,9 @@ class BirdSerializer < ActiveModel::Serializer
   def id
     object.id.to_s
   end
+
+  def added
+    o = object.added
+    "#{o.try(:year)}-#{o.try(:month)}-#{o.try(:day)}"
+  end
 end
